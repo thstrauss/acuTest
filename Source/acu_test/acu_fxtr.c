86f6797b579ed8391fb6cc62a249146a3baafdc4
Thomas Strauß <thomas.strauss@nexgo.de>
2024-03-02 00:58:03 +0100

    Move first statement behind declarations

--- Source/acu_test/acu_fxtr.c
+++ Source/acu_test/acu_fxtr.c
@@ -20,12 +20,12 @@ static void acuTest_destroyResult(ACU_Result* result) {
 
 static void acuTest_run(ACU_TestCase* testCase, const void* context) {
     ACU_ExecuteEnv environment;
-    memset(&environment, 0, sizeof(ACU_ExecuteEnv));
-
     ACU_Result* result = (ACU_Result*)acu_emalloc(sizeof(ACU_Result));
+
     result->status = ACU_TEST_PASSED;
     result->message = NULL;
 
+    memset(&environment, 0, sizeof(ACU_ExecuteEnv));
     environment.result = result;
 
     do {
