bd6e7ef75c15dc198d716b2e3a97253f22b695b1
Thomas Strauss <thomas.strauss@nexgo.de>
2024-04-06 01:31:49 +0200

    Fix NAN and register variables

--- Test/cuTester/flt_test.c
+++ Test/cuTester/flt_test.c
@@ -1,86 +1,8 @@
-#include <string.h>
-#include <setjmp.h>
+#include <string.h>#include <setjmp.h>#include <acu_fxtr.h>#include <acu_asrt.h>#include <acu_suit.h>#include <acu_util.h>#include <acu_rslt.h>#include "flt_test.h"#include <math.h>
+#include <float.h>
 
-#include <acu_fxtr.h>
-#include <acu_asrt.h>
-#include <acu_suit.h>
-#include <acu_util.h>
-#include <acu_rslt.h>
-
-#include "flt_test.h"
-#include <math.h>
-
-#define TESTBED(postfix, type, operation, actual, expetced) static void test##type##operation##postfix(ACU_ExecuteEnv* environment, const void* context) { \
-    ACU_ExecuteEnv* testEnvironment = acu_emalloc(sizeof(ACU_ExecuteEnv)); \
-    ACU_Result* resultBuf = (ACU_Result*)acu_emalloc(sizeof(ACU_Result)); \
-    testEnvironment->result = resultBuf; \
-    resultBuf->status = ACU_TEST_PASSED; \
-    resultBuf->message = NULL; \
-    resultBuf->file = NULL; \
-    if (!setjmp(testEnvironment->assertBuf)) { \
-        ACU_assert(testEnvironment, type, operation, actual, expetced, #type#operation#postfix); \
-    } \
-    TRY \
-    ACU_assert(environment, int, Equal, testEnvironment->result->status, ACU_TEST_PASSED, #type#operation#postfix); \
-    ACU_assert_strEqual(environment, testEnvironment->result->message, "", #type#operation#postfix); \
-    FINALLY \
-    if (resultBuf->message != NULL) { \
-        free(resultBuf->message); \
-    } \
-    free(resultBuf); \
-    free(testEnvironment); \
-    ETRY; \
-    UNUSED(context); \
-}\
-
-TESTBED(,float, Equal, 0.0f, 0.0f)
-TESTBED(Nan, float, NotEqual, 0.0f, NAN)
-TESTBED(,float, NotEqual, 0.0f, 1.0f)
-TESTBED(,float, Less, 0.0f, 1.0f)
-TESTBED(,float, LessEqual, 0.0f, 1.0f)
-TESTBED(Eq, float, LessEqual, 1.0f, 1.0f)
-TESTBED(,float, Greater, 1.0f, 0.0f)
-TESTBED(,float, GreaterEqual, 1.0f, 0.0f)
-TESTBED(Eq, float, GreaterEqual, 1.0f, 1.0f)
-
-TESTBED(, double, Equal, 0.0, 0.0)
-TESTBED(, double, NotEqual, 0.0, 1.0)
-TESTBED(Nan, double, NotEqual, 0.0f, NAN)
-TESTBED(, double, Less, 0.0, 1.0)
-TESTBED(, double, LessEqual, 0.0, 1.0)
-TESTBED(Eq, double, LessEqual, 1.0, 1.0)
-TESTBED(, double, Greater, 1.0, 0.0)
-TESTBED(, double, GreaterEqual, 1.0, 0.0)
-TESTBED(Eq, double, GreaterEqual, 1.0, 1.0)
-
-static void doubleNotEqual(ACU_ExecuteEnv* environment, const void* context) {
-    ACU_assert(environment, double, NotEqual, 0.0, 1.0, "");
-    UNUSED(context);
-}
-
-void floatFixture(ACU_Suite* suite)
-{
-    ACU_Fixture* fixture = acu_emalloc(sizeof(ACU_Fixture));
-    acu_suiteAddFixture(suite, fixture);
-    acu_fixtureInit(fixture, "float tests");
-
-    acu_fixtureAddTestCase(fixture, "float Equal", testfloatEqual);
-    acu_fixtureAddTestCase(fixture, "float NotEqual NAN", testfloatNotEqualNan);
-    acu_fixtureAddTestCase(fixture, "float NotEqual", testfloatNotEqual);
-    acu_fixtureAddTestCase(fixture, "float Less", testfloatLess);
-    acu_fixtureAddTestCase(fixture, "float LessEqual", testfloatLessEqual);
-    acu_fixtureAddTestCase(fixture, "float LessEqualEq", testfloatLessEqualEq);
-    acu_fixtureAddTestCase(fixture, "float Greater", testfloatGreater);
-    acu_fixtureAddTestCase(fixture, "float GreaterEqualEq", testfloatGreaterEqualEq);
-    acu_fixtureAddTestCase(fixture, "float GreaterEqual", testfloatGreaterEqual);
-
-    acu_fixtureAddTestCase(fixture, "double Equal", testdoubleEqual);
-    acu_fixtureAddTestCase(fixture, "double NotEqual NAN", testdoubleNotEqualNan);
-    acu_fixtureAddTestCase(fixture, "double NotEqual", testdoubleNotEqual);
-    acu_fixtureAddTestCase(fixture, "double Less", testdoubleLess);
-    acu_fixtureAddTestCase(fixture, "double LessEqual", testdoubleLessEqual);
-    acu_fixtureAddTestCase(fixture, "double LessEqualEq", testdoubleLessEqualEq);
-    acu_fixtureAddTestCase(fixture, "double Greater", testdoubleGreater);
-    acu_fixtureAddTestCase(fixture, "double GreaterEqualEq", testdoubleGreaterEqualEq);
-    acu_fixtureAddTestCase(fixture, "double GreaterEqual", testdoubleGreaterEqual);
-}
+#ifdef __TOS__
+#define INF FLT_MAX * 2.0f
+#define NAN INF/INF
+#endif
+#define TESTBED(postfix, type, operation, actual, expetced) static void test##type##operation##postfix(ACU_ExecuteEnv* environment, const void* context) { \    ACU_ExecuteEnv* testEnvironment = acu_emalloc(sizeof(ACU_ExecuteEnv)); \    ACU_Result* resultBuf = (ACU_Result*)acu_emalloc(sizeof(ACU_Result)); \    testEnvironment->result = resultBuf; \    resultBuf->status = ACU_TEST_PASSED; \    resultBuf->message = NULL; \    resultBuf->file = NULL; \    if (!setjmp(testEnvironment->assertBuf)) { \        ACU_assert(testEnvironment, type, operation, actual, expetced, #type#operation#postfix); \    } \    TRY \    ACU_assert(environment, int, Equal, testEnvironment->result->status, ACU_TEST_PASSED, #type#operation#postfix); \    ACU_assert_strEqual(environment, testEnvironment->result->message, "", #type#operation#postfix); \    FINALLY \    if (resultBuf->message != NULL) { \        free(resultBuf->message); \    } \    free(resultBuf); \    free(testEnvironment); \    ETRY; \    UNUSED(context); \}\TESTBED(,float, Equal, 0.0f, 0.0f)TESTBED(Nan, float, NotEqual, 0.0f, NAN)TESTBED(,float, NotEqual, 0.0f, 1.0f)TESTBED(,float, Less, 0.0f, 1.0f)TESTBED(,float, LessEqual, 0.0f, 1.0f)TESTBED(Eq, float, LessEqual, 1.0f, 1.0f)TESTBED(,float, Greater, 1.0f, 0.0f)TESTBED(,float, GreaterEqual, 1.0f, 0.0f)TESTBED(Eq, float, GreaterEqual, 1.0f, 1.0f)TESTBED(, double, Equal, 0.0, 0.0)TESTBED(, double, NotEqual, 0.0, 1.0)TESTBED(Nan, double, NotEqual, 0.0f, NAN)TESTBED(, double, Less, 0.0, 1.0)TESTBED(, double, LessEqual, 0.0, 1.0)TESTBED(Eq, double, LessEqual, 1.0, 1.0)TESTBED(, double, Greater, 1.0, 0.0)TESTBED(, double, GreaterEqual, 1.0, 0.0)TESTBED(Eq, double, GreaterEqual, 1.0, 1.0)static void doubleNotEqual(ACU_ExecuteEnv* environment, const void* context) {    ACU_assert(environment, double, NotEqual, 0.0, 1.0, "");    UNUSED(context);}void floatFixture(ACU_Suite* suite){    ACU_Fixture* fixture = acu_emalloc(sizeof(ACU_Fixture));    acu_suiteAddFixture(suite, fixture);    acu_fixtureInit(fixture, "float tests");    acu_fixtureAddTestCase(fixture, "float Equal", testfloatEqual);    acu_fixtureAddTestCase(fixture, "float NotEqual NAN", testfloatNotEqualNan);    acu_fixtureAddTestCase(fixture, "float NotEqual", testfloatNotEqual);    acu_fixtureAddTestCase(fixture, "float Less", testfloatLess);    acu_fixtureAddTestCase(fixture, "float LessEqual", testfloatLessEqual);    acu_fixtureAddTestCase(fixture, "float LessEqualEq", testfloatLessEqualEq);    acu_fixtureAddTestCase(fixture, "float Greater", testfloatGreater);    acu_fixtureAddTestCase(fixture, "float GreaterEqualEq", testfloatGreaterEqualEq);    acu_fixtureAddTestCase(fixture, "float GreaterEqual", testfloatGreaterEqual);    acu_fixtureAddTestCase(fixture, "double Equal", testdoubleEqual);    acu_fixtureAddTestCase(fixture, "double NotEqual NAN", testdoubleNotEqualNan);    acu_fixtureAddTestCase(fixture, "double NotEqual", testdoubleNotEqual);    acu_fixtureAddTestCase(fixture, "double Less", testdoubleLess);    acu_fixtureAddTestCase(fixture, "double LessEqual", testdoubleLessEqual);    acu_fixtureAddTestCase(fixture, "double LessEqualEq", testdoubleLessEqualEq);    acu_fixtureAddTestCase(fixture, "double Greater", testdoubleGreater);    acu_fixtureAddTestCase(fixture, "double GreaterEqualEq", testdoubleGreaterEqualEq);    acu_fixtureAddTestCase(fixture, "double GreaterEqual", testdoubleGreaterEqual);}
--- Test/cuTester/tryc_tst.c
+++ Test/cuTester/tryc_tst.c
@@ -1,98 +1,6 @@
-#include <acu_fxtr.h>
-#include <acu_asrt.h>
-#include <acu_suit.h>
-#include <acu_util.h>
-#include <acu_rslt.h>
-
-#include <tryctch.h>
-
-#include "tryc_tst.h"
-
-static void tryETryTests(ACU_ExecuteEnv* environment, const void* context) {
-    int visited = 0;
-    TRY
-        visited = 1;
-    ETRY;
-    ACU_assert(environment, int, Equal, visited, 1, "block not visited");
-    UNUSED(context);
-}
-
-static void tryCatchNotVisitedTests(ACU_ExecuteEnv* environment, const void* context) {
-    int visited = 0;
-    int catched = 0;
-    TRY
-        visited = 1;
-    CATCH(1)
-        catched = 1;
-    ETRY;
-    ACU_assert(environment, int, Equal, visited, 1, "block not visited");
-    ACU_assert(environment, int, Equal, catched, 0, "catch not visited");
-    UNUSED(context);
-}
-
-static void tryCatchVisitedTests(ACU_ExecuteEnv* environment, const void* context) {
-    int visited = 0;
-    int catched = 0;
-    TRY
-        visited++;
-        THROW(1);
-        visited++;
-    CATCH(1)
-        catched = 1;
-    ETRY;
-    ACU_assert(environment, int, Equal, visited, 1, "block visited");
-    ACU_assert(environment, int, Equal, catched, 1, "catch not visited");
-    UNUSED(context);
-}
-
-static void tryCatchVisitedExpandedTests(ACU_ExecuteEnv* environment, const void* context) {
-    int visited = 0;
-    int catched = 0;
-    do {
-        jmp_buf _exception_Buf; switch (_setjmp(_exception_Buf)) {
-        case 0: while (1) {
-            visited++;
-            longjmp(_exception_Buf, (1));
-            visited++;
-        break; case (1):
-            catched = 1;
-            break;
-        }
-        }
-    } while (0);
-    ACU_assert(environment, int, Equal, visited, 1, "block visited");
-    ACU_assert(environment, int, Equal, catched, 1, "catch not visited");
-    UNUSED(context);
-}
-
-
-static void tryCatchFinallyVisitedTests(ACU_ExecuteEnv* environment, const void* context) {
-    int visited = 0;
-    int catched = 0;
-    int finally = 0;
-    TRY
-        THROW(1);
-        visited = 1;
-    CATCH(1)
-        catched = 1;
-    FINALLY
-        finally = 1;
-    ETRY;
-    ACU_assert(environment, int, Equal, visited, 0, "block visited");
-    ACU_assert(environment, int, Equal, catched, 1, "catch not visited");
-    ACU_assert(environment, int, Equal, finally, 1, "finally not visited");
-    UNUSED(context);
-}
-
-void tryCatchFixture(ACU_Suite* suite)
-{
-    ACU_Fixture* fixture = acu_emalloc(sizeof(ACU_Fixture));
-    acu_suiteAddFixture(suite, fixture);
-    acu_fixtureInit(fixture, "try catch finally");
-
-    acu_fixtureAddTestCase(fixture, "try etry", tryETryTests);
-    acu_fixtureAddTestCase(fixture, "try catch", tryCatchNotVisitedTests);
-    acu_fixtureAddTestCase(fixture, "try throw catch", tryCatchVisitedTests);
-    acu_fixtureAddTestCase(fixture, "try throw catch_ expanded", tryCatchVisitedExpandedTests);
-    acu_fixtureAddTestCase(fixture, "try throw catch finally", tryCatchFinallyVisitedTests);
-}
+#include <acu_fxtr.h>#include <acu_asrt.h>#include <acu_suit.h>#include <acu_util.h>#include <acu_rslt.h>#include <tryctch.h>#include "tryc_tst.h"static void tryETryTests(ACU_ExecuteEnv* environment, const void* context) {    int visited = 0;    TRY        visited = 1;    ETRY;    ACU_assert(environment, int, Equal, visited, 1, "block not visited");    UNUSED(context);}static void tryCatchNotVisitedTests(ACU_ExecuteEnv* environment, const void* context) {    int visited = 0;    int catched = 0;    TRY        visited = 1;    CATCH(1)        catched = 1;    ETRY;    ACU_assert(environment, int, Equal, visited, 1, "block not visited");    ACU_assert(environment, int, Equal, catched, 0, "catch not visited");    UNUSED(context);}static void tryCatchVisitedTests(ACU_ExecuteEnv* environment, const void* context) {    int visited = 0;    int catched = 0;    TRY        visited++;        THROW(1);        visited++;    CATCH(1)        catched = 1;    ETRY;    ACU_assert(environment, int, Equal, visited, 1, "block visited");    ACU_assert(environment, int, Equal, catched, 1, "catch not visited");    UNUSED(context);}static void tryCatchVisitedExpandedTests(ACU_ExecuteEnv* environment, const void* context) { 
+	struct {
+		int catched;
+		int visited;
+	} x;    x.catched = 0;
+    x.visited = 0;    do {        jmp_buf _exception_Buf; switch (setjmp(_exception_Buf)) {        case 0: while (1) {            x.visited++;            longjmp(_exception_Buf, (1));            x.visited++;        break; case (1):            x.catched = 1;            break;        }        }    } while (0);    ACU_assert(environment, int, Equal, x.visited, 1, "block visited");    ACU_assert(environment, int, Equal, x.catched, 1, "catch not visited");    UNUSED(context);}static void tryCatchFinallyVisitedTests(ACU_ExecuteEnv* environment, const void* context) {    int visited = 0;    int catched = 0;    int finally = 0;    TRY        THROW(1);        visited = 1;    CATCH(1)        catched = 1;    FINALLY        finally = 1;    ETRY;    ACU_assert(environment, int, Equal, visited, 0, "block visited");    ACU_assert(environment, int, Equal, catched, 1, "catch not visited");    ACU_assert(environment, int, Equal, finally, 1, "finally not visited");    UNUSED(context);}void tryCatchFixture(ACU_Suite* suite){    ACU_Fixture* fixture = acu_emalloc(sizeof(ACU_Fixture));    acu_suiteAddFixture(suite, fixture);    acu_fixtureInit(fixture, "try catch finally");    acu_fixtureAddTestCase(fixture, "try etry", tryETryTests);    acu_fixtureAddTestCase(fixture, "try catch", tryCatchNotVisitedTests);    acu_fixtureAddTestCase(fixture, "try throw catch", tryCatchVisitedTests);    acu_fixtureAddTestCase(fixture, "try throw catch_ expanded", tryCatchVisitedExpandedTests);    acu_fixtureAddTestCase(fixture, "try throw catch finally", tryCatchFinallyVisitedTests);}
