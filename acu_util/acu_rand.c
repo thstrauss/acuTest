9f9f77e6a4c05c7aafecc65b9eff1f02bdff84fe
Thomas Strauss <thomas.strauss@nexgo.de>
2024-06-03 18:39:44 +0200

    Add copyright

--- acu_util/acu_rand.c
+++ acu_util/acu_rand.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2024 Thomas Strauss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall
+ * be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 /*  Written in 2018 by David Blackman and Sebastiano Vigna (vigna@acm.org)
 
 To the extent possible under law, the author has dedicated all copyright
@@ -5,8 +26,6 @@ and related and neighboring rights to this software to the public domain
 worldwide. This software is distributed without any warranty.
 
 See <http://creativecommons.org/publicdomain/zero/1.0/>. */
-
-#include "acu_rand.h"
 
 /* This is xoshiro128** 1.1, one of our 32-bit all-purpose, rock-solid
    generators. It has excellent speed, a state size (128 bits) that is
@@ -20,6 +39,8 @@ See <http://creativecommons.org/publicdomain/zero/1.0/>. */
    numbers, xoshiro128+ is even faster.
 
    The state must be seeded so that it is not everywhere zero. */
+
+#include "acu_rand.h"
 
 #ifdef __TOS__
 static uint32_t rotl(const uint32_t x, int k) 0xE3B8;
