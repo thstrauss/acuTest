8eccd80e168def6239156cab0df8032cce1ab537
Thomas Strauss <thomas.strauss@nexgo.de>
2024-10-26 12:31:47 +0200

    experiment on faster strlen

--- cutest.lib/misc_tst.c
+++ cutest.lib/misc_tst.c
@@ -10,6 +10,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 
 static float func2(float f1, ...) {
     va_list list;
@@ -48,17 +49,21 @@ static void orphanedAlloc(ACU_ExecuteEnv* environment, const void* context) {
     UNUSED(context);
 }
 
-int zbyter(unsigned long x) {
+static int zbyter(unsigned long x) {
     unsigned long y;
 
     y = (x & 0x7F7F7F7FL) + 0x7F7F7F7FL;
     y = ~(y | x | 0x7F7F7F7FL);        
 
-    if (y == 0) return 4;              
-    else if (y & 0x0000FFFFL)          
-        return (int) ((y >> 7) & 1) ^ 1;     
-    else                               
-        return (int) ((y >> 23) & 1) ^ 3;    
+    if (y == 0) {
+        return 4;
+    } 
+    else if (y & 0x0000FFFFL) {
+        return (int) ((y >> 7) & 1) ^ 1;
+    }
+    else {
+        return (int) ((y >> 23) & 1) ^ 3;
+    }
 }
 
 
@@ -74,18 +79,107 @@ static int zbytel(unsigned long x) {
         return (int) (y >> 15) ^ 3;
 }
 
+
+static size_t fstrlen_C(const char* s) {
+    const char* sPtr = s;
+    unsigned long  magic = 0x7F7F7F7FL;
+
+    unsigned long  l = *((const unsigned long *)sPtr);
+    while (!~(((l & magic) + magic) | l | magic)) {
+        sPtr += sizeof(unsigned long );
+        l = *((const unsigned long *)sPtr);
+        if (~(((l & magic) + magic) | l | magic)) {
+            break;
+        }
+        sPtr += sizeof(unsigned long);
+        l = *((const unsigned long*)sPtr);
+        if (~(((l & magic) + magic) | l | magic)) {
+            break;
+        }
+        sPtr += sizeof(unsigned long);
+        l = *((const unsigned long*)sPtr);
+    }
+    while (*sPtr) {
+        if (!*(++sPtr)) break;
+        if (!*(++sPtr)) break;
+        sPtr++;
+    }
+    return sPtr - s;
+}
+
+typedef void TestFunc(void);
+
+static unsigned long measureLoop(clock_t duration, TestFunc* func) {
+    unsigned long count = 0;
+    clock_t end = clock() + duration;
+    while (clock() < end) {
+        count++;
+        func();
+    }
+    return count;
+}
+
+static size_t x  = 0;
+
+static void voidFunc(void) {
+	int i;
+	size_t s=0;
+	for (i=0; i<10000; i++) {
+        s = 0;
+	}
+    x = s;
+	UNUSED(s);
+}
+
+static void strLenFunc(void) {
+	int i;
+	size_t s=0;
+	for (i=0; i<10000; i++) {
+    	s = strlen("The quick brown fox jumps over the lazy dog");
+    }
+    x = s;
+    UNUSED(s);
+}
+
+#ifdef __TOS__
+extern size_t fstrlen(const char*);
+
+static void fstrLenFunc(void) {
+    int i;
+	size_t s = 0;
+	for (i=0; i<10000; i++) {
+		fstrlen("The quick brown fox jumps over the lazy dog");
+    }
+    UNUSED(s);
+    }
+#endif
+
+static void fstrlen_CFunc(void) {
+    int i;
+	size_t s = 0;
+	for (i=0; i<10000; i++) {
+		s = fstrlen_C("The quick brown fox jumps over the lazy dog");
+    }
+    x = s;
+    UNUSED(s);
+}
+
+
 static void strlenTest(ACU_ExecuteEnv* environment, const void* context) {
-    char x0[] = "";
-    char x1[] = "1";
-    char x2[] = "12";
-    char x3[] = "123";
-    char x4[] = "1234";
-/*    printf("%d %d %08lx\n\r", zbyter(*(unsigned long*)x0), zbytel(*(unsigned long*)x0), *(unsigned long*)x0);
-    printf("%d %d %08lx\n\r", zbyter(*(unsigned long*)x1), zbytel(*(unsigned long*)x1), *(unsigned long*)x1);
-    printf("%d %d %08lx\n\r", zbyter(*(unsigned long*)x2), zbytel(*(unsigned long*)x2), *(unsigned long*)x2);
-    printf("%d %d %08lx\n\r", zbyter(*(unsigned long*)x3), zbytel(*(unsigned long*)x3), *(unsigned long*)x3);
-    printf("%d %d %08lx\n\r", zbyter(*(unsigned long*)x4), zbytel(*(unsigned long*)x4), *(unsigned long*)x4);
-*/
+
+    #define DIVISOR 5
+    printf("voidFunc\t%ld\n\r", (measureLoop(CLK_TCK / DIVISOR, voidFunc))*DIVISOR);
+    printf("strLenFunc\t%ld\n\r", (measureLoop(CLK_TCK / DIVISOR, strLenFunc))*DIVISOR);
+#ifdef __TOS__
+    printf("fstrLenFunc\t%ld\n\r", (measureLoop(CLK_TCK / DIVISOR , fstrLenFunc))*DIVISOR);
+#endif
+    printf("fstrlen_CFunc\t%ld\n\r", (measureLoop(CLK_TCK / DIVISOR, fstrlen_CFunc))*DIVISOR);
+
+    printf("%ld\n\r", (long) fstrlen_C("The quick brown fox jumps over the lazy dog"));
+
+#ifdef __TOS__
+    printf("%ld\n\r", fstrlen("The quick brown fox jumps over the lazy dog"));
+#endif
     UNUSED(environment);
     UNUSED(context);
 }
