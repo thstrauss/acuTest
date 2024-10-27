b155b643caa9e21628b4fb755727dcaaa842dd83
Thomas Strauss <thomas.strauss@nexgo.de>
2024-10-27 23:20:14 +0100

    Insert NOP to write back to memory

--- acu_util/tos/acu_cach.s
+++ acu_util/tos/acu_cach.s
@@ -1,35 +1,27 @@
 .TEXT
 
-        EXPORT __clearInstructionCache
         EXPORT __disableInstructionCache
-		EXPORT __enableInstructionCache
-		EXPORT __getCacr
-
-__clearInstructionCache:
-
-		MOVEC cacr, d0
-		ORI.W #$08, d0
-		MOVEC d0, cacr
-		
-		RTS
+        EXPORT __enableInstructionCache
+        EXPORT __getCacr
 
 __disableInstructionCache:
-		MOVEC cacr, d0
-		ANDI.W  #$f6f6, d0
-		MOVEC d0, cacr
-		
-		RTS
+        NOP
+        MOVEC cacr, d0
+        ANDI.W  #$f6f6, d0
+        MOVEC d0, cacr
+        
+        RTS
 
 __enableInstructionCache:
-
-		MOVEC cacr, d0
-		ORI.W  #$0909, d0
-		MOVEC d0, cacr
-		
-		RTS
+        NOP
+        MOVEC cacr, d0
+        ORI.W  #$0909, d0
+        MOVEC d0, cacr
+        
+        RTS
 
 __getCacr:
 
-		MOVEC cacr, d0
-		
-		RTS
+        MOVEC cacr, d0
+        
+        RTS
