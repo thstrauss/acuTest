3b1fb9370526035e3280eaf226621ffb3c394ada
Thomas Strauss <thomas.strauss@nexgo.de>
2025-02-15 00:18:03 +0100

    Complete API description for fixtures.

--- acu_test/acu_fxtr.h
+++ acu_test/acu_fxtr.h
@@ -33,7 +33,7 @@
 
 struct ACU_List_;
 
-/* The ACU_Fixture allows to group ACU_TestCase's. */
+/* The ACU_Fixture structure allows to group ACU_TestCase's and to hold a hierarchy of fixtures. */
 typedef struct ACU_Fixture_ {
     struct ACU_List_* testCases;
     struct ACU_List_* childFixtures;
@@ -43,28 +43,52 @@ typedef struct ACU_Fixture_ {
     clock_t duration;
 } ACU_Fixture;
 
-/* Allocates memory for an ACU_Fixture. */
+/* Allocates memory for an ACU_Fixture. 
+* @return The allocated memory.
+* @remark The memory must be freed with acu_freeFixture.
+*/
 #define acu_mallocFixture() ((ACU_Fixture*) acu_emalloc(sizeof(ACU_Fixture)))
 
-/* Initializes an ACU_Fixture.*/
+/* Initializes an ACU_Fixture.
+* @param fixture The fixture to initialize.
+* @param name The name of the fixture.
+*/
 __EXPORT void acu_initFixture(ACU_Fixture* fixture, const char* name);
 
-/* Destroys an ACU_Fixture. */
+/* Destroys an ACU_Fixture. 
+* @param fixture The fixture to destroy.
+*/
 __EXPORT void acu_fixtureDestroy(ACU_Fixture* fixture);
 
-/* Adds a test case to the fixture. */ 
+/* Adds a test case to the fixture. 
+ * @param fixture The fixture to add the test case to.
+ * @param name The name of the test case.
+ * @param testFunc The function to execute the test case.
+ */ 
 __EXPORT void acu_addTestCase(ACU_Fixture* fixture, const char *name, ACU_TestFunc testFunc);
 
-/* Adds a child fixture to the fixture. */ 
+/* Adds a child fixture to the fixture.
+ * @param fixture The fixture to add the child fixture to.
+ * @param childFixture The child fixture to add.
+ */ 
 __EXPORT void acu_addChildFixture(ACU_Fixture* fixture, ACU_Fixture* childFixture);
 
-/* Sets the context for the fixture. */
+/* Sets the context for the fixture. The context is passed to the test cases when executed.
+ * @param fixture The fixture to set the context for.
+ * @param context The context to set.
+ */
 __EXPORT void acu_setFixtureContext(ACU_Fixture* fixture, const void* context);
 
-/* Executes the fixture. */
+/* Executes the fixture. 
+ * @param fixture The fixture to execute.
+ * @param progress The progress to report to.
+ */
 __EXPORT enum ACU_TestResult acu_executeFixture(ACU_Fixture* fixture, ACU_Progress* progress);
 
-/* Accepts a fixture. */ 
+/* Accepts a fixture. All Nodes of the fixture are visited.
+ * @param fixture The fixture to accept.
+ * @param visitor The visitor to accept the fixture.
+ */ 
 __EXPORT void acu_acceptFixture(const ACU_Fixture* fixture, ACU_ReportVisitor* visitor);
 
 #endif
